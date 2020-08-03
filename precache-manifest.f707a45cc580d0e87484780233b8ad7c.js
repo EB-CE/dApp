d87796fd5deee704f2ccccf9a501de9b1a1d08a0
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-08-04 02:37:43 +0430

    Updates

---
+++ precache-manifest.f707a45cc580d0e87484780233b8ad7c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48512c416fc52491162424e7a3f4fe4f",
+    "url": "/LedgerPrototypedApp/index.html"
+  },
+  {
+    "revision": "f91ddb70802638c3b44d",
+    "url": "/LedgerPrototypedApp/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "970af0c48481d7dd982b",
+    "url": "/LedgerPrototypedApp/static/css/main.8fa8a52d.chunk.css"
+  },
+  {
+    "revision": "f91ddb70802638c3b44d",
+    "url": "/LedgerPrototypedApp/static/js/2.b3030b5f.chunk.js"
+  },
+  {
+    "revision": "d76c01a4c84bcbc8056ea7391963e9f6",
+    "url": "/LedgerPrototypedApp/static/js/2.b3030b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970af0c48481d7dd982b",
+    "url": "/LedgerPrototypedApp/static/js/main.945fa8bb.chunk.js"
+  },
+  {
+    "revision": "c0ee7708dc72aaa3df1f",
+    "url": "/LedgerPrototypedApp/static/js/runtime-main.62fb5144.js"
+  },
+  {
+    "revision": "0b1490ddb9b44499265cadb25a7b33fd",
+    "url": "/LedgerPrototypedApp/static/media/bg2.0b1490dd.png"
+  },
+  {
+    "revision": "b0d6fa0093865cad7c48d1c8ff6ccbed",
+    "url": "/LedgerPrototypedApp/static/media/mylogo.b0d6fa00.jpg"
+  },
+  {
+    "revision": "0f04b257c666e13481655674eb6ad85d",
+    "url": "/LedgerPrototypedApp/static/media/uulogo.0f04b257.png"
+  }
+]);
